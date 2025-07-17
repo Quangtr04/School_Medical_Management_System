ad3947f6ae1a3cee8deed8a0a76ce1d7ca88264e
duyquangn84@gmai.com <duyquangn84@gmail.com>
2025-07-17 22:11:55 +0700

    Cập nhật BE: sửa index.js và cập nhật ảnh uploads

--- BE/src/index.js
+++ BE/src/index.js
@@ -18,10 +18,11 @@ require("dotenv").config();
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "http://192.168.1.168:5173",
+    "http://192.168.1.6:5173",
     "http://192.168.1.160:5173",
     "http://172.20.10.4:5173",
     "http://172.20.10.2:5173",
+
   ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   optionsSuccessStatus: 204,
